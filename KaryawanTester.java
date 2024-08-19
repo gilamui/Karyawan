a59549be2192d3b73cb1e1f5e6e2d9f408bb753e
gilamui <mattew_marante_33rpl@student.smktelkom-mlg.sch.id>
2024-08-19 12:08:19 +0700

    Update KaryawanTester.java

--- KaryawanTester.java
+++ KaryawanTester.java
@@ -3,11 +3,17 @@ public class KaryawanTester {
         Karyawan Moreno = new Karyawan(24,"Moreno","TU","Bogor");
         Karyawan Nibras = new Karyawan(29, "Nibras", "Kesiswaan", "lamongan");
         Karyawan Burhan = new Karyawan(26, "Burhan", "Kurikulum", "Tulungagung");
+        Karyawan Bintang = new Karyawan(25, "Bintang", "Kesiswaan", "Inggris");
+        Karyawan Azam = new Karyawan(22, "Azam", "Kurikulum", "Malang");
 
         Moreno.print();
         System.out.println();
         Nibras.print();
         System.out.println();
         Burhan.print();
+        System.out.println();
+        Bintang.print();
+        System.out.println();
+        Azam.print();
     }
 }
